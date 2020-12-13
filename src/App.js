aea989e7f8d4306bd7d65a8abf647a395cc1b2d1
Jorge <jorgediazok@gmail.com>
2020-12-12 21:51:33 -0300

    added contentful connection

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import React, { useEffect } from 'react';
 import './App.css';
+import Client from './api';
+
+const getData = async () => {
+  try {
+    let response = await Client.getEntries({
+      content_type: 'footballShirts',
+    });
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 function App() {
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="App">
-     <h1>Footbal App</h1>
+      <h1>Footbal App</h1>
+      <div>{}</div>
     </div>
   );
 }
